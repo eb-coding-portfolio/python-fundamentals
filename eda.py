c0ecb925008a70e804182723fdef24c775f3f9e7
Eric Balduf <ebalduf2017@gmail.com>
2023-06-12 19:07:26 -0400

    finished scatter plot, normalizing variables, and updated all notes in ed_notebook

--- eda.py
+++ eda.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import zscore
+
 
 file_path = r'C:\Users\Eric C. Balduf\OneDrive\Documents\my-local-repo\python-fundamentals\Data\Redfin\redfin_metro_market_tracker.tsv000'
+excel_path = r'C:\Users\Eric C. Balduf\OneDrive\Documents\\'
 
 df_metro = pd.read_csv(file_path, delimiter='\t')
+
+df_metro_ex_all_res = df_metro[df_metro['property_type'] != 'All Residential']
+
+
+#get a summarization of all of the data columns. Be sure to include:
 
 summary_stats = df_metro.describe().round(2)
 
@@ -14,6 +22,7 @@ desired_stats = summary_stats.loc[['min', '25%', '50%', '75%', 'max']]
 
 columns_to_drop = ['period_duration', 'region_type_id', 'table_id', 'city', 'state', 'property_type_id', 'parent_metro_region_metro_code']
 
+#min/25/50/75th/max for all numeric variables
 desired_stats_final = desired_stats.drop(columns=columns_to_drop)
 
 column_types = df_metro.dtypes
@@ -24,20 +33,21 @@ df_metro[date_columns] = df_metro[date_columns].apply(pd.to_datetime)
 
 column_types = df_metro.dtypes
 
-desired_stats_final.to_csv(r'C:\Users\Eric C. Balduf\OneDrive\Documents\df_metro_summary_stats.csv', index_label='Stat')
 
-excel_writer = pd.ExcelWriter(r'C:\Users\Eric C. Balduf\OneDrive\Documents\value_counts_results.xlsx', engine='openpyxl')
+excel_writer = pd.ExcelWriter(f'{excel_path}eda_results.xlsx', engine='openpyxl')
 
+desired_stats_final.to_excel(excel_writer, sheet_name="desired_stats_final", index=True)
 
-
+#for each categorical variable that isn't a unique identifier,get value counts
 for column in df_metro.columns:
     if pd.api.types.is_object_dtype(column_types[column]) or pd.api.types.is_categorical_dtype(column_types[column]):
         value_counts = df_metro[column].value_counts().reset_index()
-        sheet_name = column
+        sheet_name = f"{column}_val_cnt"
         value_counts.to_excel(excel_writer, sheet_name=sheet_name, index=False)
 
 excel_writer._save()
 
+#number of null / na values in each column
 null_counts = df_metro.isnull().sum()
 null_counts = null_counts.reset_index()
 null_counts.columns = ['Column', 'Count']
@@ -54,13 +64,14 @@ null_counts_pct.to_excel(excel_writer, sheet_name="null_counts_pct", index=False
 
 excel_writer._save()
 
+#for all numeric variables, plot a single correlation matrix
 num_list = []
-for col in df_metro.columns:
+for col in df_metro_ex_all_res.columns:
 
-    if pd.api.types.is_numeric_dtype(df_metro[col]):
+    if pd.api.types.is_numeric_dtype(df_metro_ex_all_res[col]):
         num_list.append(col)
 
-subset_corr_df = df_metro[num_list].drop(columns=columns_to_drop)
+subset_corr_df = df_metro_ex_all_res[num_list].drop(columns=columns_to_drop)
 
 exclude_columns = [col for col in subset_corr_df.columns if 'mom' in col or 'yoy' in col]
 
@@ -70,14 +81,33 @@ subset_corr_df_filtered = subset_corr_df.loc[:, ~subset_corr_df.columns.isin(exc
 correlation_matrix = subset_corr_df_filtered.corr()
 
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-
+plt.show()
 plt.title('Correlation Matrix')
 
+#for all categorical variables, plot a bar chart
+##NEED WORK
 
+#Plot a scatter plot
+sns.scatterplot(df_metro_ex_all_res, x='inventory', y='median_sale_price', hue="property_type")
+plt.title('Scatter Plot Inventory vs. Median Sale Price')
 plt.show()
 
 
-#Next    4. for all categorical variables, plot a bar chart
+
+
+#normalize a variable
+df_metro_ex_all_res_copy = df_metro_ex_all_res.copy()
+
+df_metro_ex_all_res_copy['inventory_normalized'] = zscore(df_metro_ex_all_res_copy['inventory'].dropna())
+df_metro_ex_all_res_copy['median_sale_price_normalized'] = zscore(df_metro_ex_all_res_copy['median_sale_price'].dropna())
+
+#If there are any free text fields, note them for future exploration
+#load a second data set and find a way to join it to another dataset
+
+
+
+
+
 
 
 
--- eda_notebook.md
+++ eda_notebook.md
@@ -4,13 +4,21 @@ Notes
 Dataset Selected: redfin_metro_market_tracker.tsv000  
 
 **min/25/50/75th/max for all numeric variables**  
-Observations --> -1 seems to be a min in a lot of the numerical columns, is this perhaps suppose to signify something else? Doesnt seem like a real value. Also some of the values seems unrealistically low, there is a value of 400 for a min median sales price which seems inaccurate.  
-
+* -1 seems to be a min in a lot of the numerical columns, is this perhaps suppose to signify something else? Doesnt seem like a real value. Also some of the values seems unrealistically low, there is a value of 400 for a min median sales price which seems inaccurate.  
+* 
 **for each categorical variable that isn't a unique identifier, get value counts**  
-Add notes  
-  
+* region type - There is only one value for which is 'metro' which makes sense because I am using the metro file from redfin
+* is_seasonally_adjusted - There is only one value for which is 'f'
+  * Does that mean seasonally adjust or not seasonally adjusted? ~Question to answer  
+* region - I see some of the regions have a count of 680 which makes sense given the time series is a little over 11 years. The periodicity is 1 month and there are 5 property types for each month. 
+  * Some are shorter than 680, why is that and how do I ensure everything is the same length? Does it need to be the same length? ~Question to answer
+* state_code - looks good, 50 states + 1 for Washington DC
+* property_type - 5 values: All Residential, Single Family Residential, Multi-Family (2-4 Unit), & Townhouse. Seems like there are a lot more single family residential records. 
+* parent_metro_region - N/A
+
 **number of null / na values in each column**  
-Add notes  
+city, state, price_drops_yoy, price_drops_mom, & price_drops are all over 50% null/NA with city and state being completely empty
+
   
 **for all numeric variables, plot a single correlation matrix**  
 Add Notes
